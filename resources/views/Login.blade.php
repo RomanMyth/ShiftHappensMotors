b0932a44101af58835953da0c381a6149bd9aa71
Franklinn19 <117764814+Franklinn19@users.noreply.github.com>
2024-02-09 14:40:05 -0500

    Changed head sections with Boostrap , new buttons on Login and Register that takes the user to the other are working at the end of each page

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -1,21 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
+    <head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+        <style>
+            .col-lg-3{
+                display: flex;
+                flex-direction: column;
+            }
+        </style>
+    </head>
 <body>
     <div>
-        <h3>Login</h3>
-        <form action="" method="POST">
-            
+        <form action=""method="POST" class=".container p-5">
+            <h3>Login</h3>
             <input type ="text" name="emailID" placeholder="Email" required/><br/>
             <input type="password" name="userPass" placeholder="Password" required/><br/>
                 <button type="submit">Login</button>
         </form>
-        <form action="" method="">
+        <form action="{{url('/Register')}}" method="" class=".container p-5">
             
             <p>Not Registered?</p>
             <button type="submit">Register Now</button>
