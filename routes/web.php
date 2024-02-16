2541c1f517a11bbcbd7c714670bd2010e32374ef
tpob11 <tpobursky21@gmail.com>
2024-02-16 12:22:38 -0500

    schedule maintenance design

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\CarControllerAPI;
+use App\Http\Controllers\MaintenanceControllerAPI;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,3 +35,5 @@ Route::get('/test', [TestController::class, 'getTestData']);
 
 Route::get("/addVehicle", [CarControllerAPI::class, 'addVehicleForm']);
 Route::post("/storeVehicle", [CarControllerAPI::class, 'store']);
+
+Route::get('/scheduleMaintenance', [MaintenanceControllerAPI::class, 'index']);
