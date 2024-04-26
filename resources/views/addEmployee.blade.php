a9d782a382922659064d6e145a97eb8e32fe8637
tpob11 <tpobursky21@gmail.com>
2024-04-26 12:50:39 -0400

    updated styling for login, reg, add emp and vehicle, schedule maintenance

--- resources/views/addEmployee.blade.php
+++ resources/views/addEmployee.blade.php
@@ -15,14 +15,121 @@
             display: flex;
             flex-direction: column;
         }
+        .empSelect {
+    width: 416px;
+    /* padding: 10px; */
+    /* margin-bottom: 15px; */
+    border: 1px solid #ccc;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+    box-sizing: border-box;
+    background-color: #fff;
+    color: #495057;
+    line-height: 1;
+    /* appearance: none; */
+    height: 28px; /* Adjust height to match input fields */
+    /* position: relative; */
+}
+
+
+.empSelect option {
+    padding: 10px;
+}
+
+
+.empSelect:hover {
+    border-color: #007bff;
+}
+
+.empSelect:focus {
+    border-color: #007bff;
+    box-shadow: 0 0 5px rgba(0, 123, 255, 0.5);
+}
+
+.container {
+                max-width: 800px; /* Limit the width of the container */
+            margin: 0 auto; /* Center the container horizontally */
+            background-color: #f9f9f9;
+            padding: 20px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 30px;
+            }
+
+            h2 {
+                text-align: center;
+            margin-bottom: 30px;
+            }
+
+
+
+            input[type="text"],
+input[type="email"],
+input[type="date"], {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-sizing: border-box;
+}
+
+input[type="text"],
+input[type="email"],
+input[type="date"],
+textarea {
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+    margin-bottom: 20px;
+}
+
+input[type="text"]:hover,
+input[type="email"]:hover,
+input[type="date"]:hover,
+textarea:hover {
+    border-color: #007bff; /* Change border color on hover */
+}
+
+input[type="text"]:focus,
+input[type="email"]:focus,
+input[type="date"]:focus,
+textarea:focus {
+    border-color: #007bff; /* Change border color on focus */
+    box-shadow: 0 0 5px rgba(0, 123, 255, 0.5); /* Add box shadow on focus */
+}
+form {
+    background-color: #f9f9f9;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    margin-bottom: 50px;
+    margin-top: 50px;
+}
+
+
+label {
+    font-weight: bold;
+}
+
+#submitBtn {
+    font-weight: bold;
+}
+
+h2 {
+                text-align: center;
+            margin-bottom: 30px;
+            }
+
+
     </style>
 </head>
 <body>
     <x-navbar>
     </x-navbar>
-    
-    <div class="emp-container p-5">
-        <h1>Add Employee</h1>
+
+    <div id="empBack" class="emp-container p-5">
+        <h2>Add Employee</h2>
         <form action="{{ route('employees.store') }}" method="POST" class="container">
             @csrf
             <div class="row">
@@ -46,7 +153,7 @@
                 </div>
                 <div class="col-lg-4">
                     <label for="userType">Department:</label>
-                    <select name="userType">
+                    <select class="empSelect" name="userType">
                         <option value="Manager">Manager</option>
                         <option value="Salesperson">Salesperson</option>
                         <option value="Technician">Technician</option>
@@ -68,7 +175,7 @@
                     <input type="date" name="DOB">
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+            <button id="submitBtn" type="submit" class="btn btn-primary mt-3">Submit</button>
         </form>
     </div>
 </body>
